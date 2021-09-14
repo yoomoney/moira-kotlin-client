b35104c2ce8fdb6cd32b021c4fb8fa92d8268ca1
karpenkovk <karpenkovk@yoomoney.ru>
2021-09-15 00:48:56 +0300

    added kotlin-plugin

--- src/test/kotlin/ru/yoomoney/tech/moira/client/AbstractMoiraTest.kt
+++ src/test/kotlin/ru/yoomoney/tech/moira/client/AbstractMoiraTest.kt
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.TestInstance
-import ru.yoomoney.tech.moira.client.Moira
 import ru.yoomoney.tech.moira.client.settings.MoiraSettings
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
